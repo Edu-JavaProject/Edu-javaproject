cbeb34cb4d59915807183dba86cf98d267e60370
highsky134 <sangchun134@gmail.com>
2023-04-12 15:32:33 +0900

    230413 아카데미 수정

--- src/choongang/academy/searchLecList.java
+++ src/choongang/academy/searchLecList.java
@@ -1,7 +1,8 @@
 package choongang.academy;
 
 public class searchLecList extends LectureManagement {
-    public searchLecList(String java_입문, String 홍길동, int i, int i1, int i2) {
-
+    public searchLecList(String lectureName, String teacherName,
+                         int lectureDate, int lectureFee) {
+        super(lectureName, teacherName, lectureDate, lectureFee);
     }
 }
