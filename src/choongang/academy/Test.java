8aa522ce1a621b6ee6d1ad9b37a808accf81328c
highsky134 <sangchun134@gmail.com>
2023-04-12 15:32:33 +0900

    이사 후 청소 완료

---
+++ src/choongang/academy/Test.java
@@ -0,0 +1,8 @@
+package choongang.academy;
+
+public class Test {
+    public static void main(String[] args) {
+        AcademyRepository ar = new AcademyRepository();
+        ar.getLectureList();
+    }
+}
