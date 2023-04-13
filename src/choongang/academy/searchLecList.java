a22f9a16450c5d586e1aadb1609a5746c2b4aaee
highsky134 <sangchun134@gmail.com>
2023-04-13 15:18:10 +0900

    230413 academy

--- src/choongang/academy/searchLecList.java
+++ src/choongang/academy/searchLecList.java
@@ -2,7 +2,7 @@ package choongang.academy;
 
 public class searchLecList extends LectureManagement {
     public searchLecList(String lectureName, String teacherName,
-                         int lectureDate, int lectureFee) {
-        super(lectureName, teacherName, lectureDate, lectureFee);
+                         int lectureDate, int lectureFee, int countStudent) {
+        super(lectureName, teacherName, lectureDate, lectureFee, countStudent);
     }
 }
