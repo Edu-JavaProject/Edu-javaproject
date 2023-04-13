7c01b7db232e6262caf19bfedfe009d9f5475a1c
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-04-13 21:29:13 +0900

    new fianan 날짜 형변환 및 검증 추가

--- src/choongang/student/Main.java
+++ src/choongang/student/Main.java
@@ -1,15 +1,15 @@
-//package choongang.student;
+package choongang.student;
+
+public class Main {
+    public static void main(String[] args) {
+//        StudentRepository sr = new StudentRepository();
+//        boolean b = sr.registerStudent(new Student("test", "1234", "홍길동"
+//                , "abc@mail.com", "24", Gender.MALE, 200000));
 //
-//public class Main {
-//    public static void main(String[] args) {
-////        StudentRepository sr = new StudentRepository();
-////        boolean b = sr.registerStudent(new Student("test", "1234", "홍길동"
-////                , "abc@mail.com", "24", Gender.MALE, 200000));
-////
-////        System.out.println("b = " + b);
-//
-//        StudentView studentView = new StudentView();
-//        studentView.start();
-//
-//    }
-//}
+//        System.out.println("b = " + b);
+
+        StudentView studentView = new StudentView();
+        studentView.start();
+
+    }
+}
